6a840edd90220130b2a16d834bf10d8fc4af08ce
ab5562 <ab5562@nyu.edu>
2017-02-05 12:49:08 -0500

    Initial Commite

---
+++ js/scripts.js
@@ -0,0 +1,120 @@
+var oneisGray = true;
+console.log(oneisGray);
+$('.firstbox').on('click', function() {
+	if (oneisGray == true) {
+  	$('.firstbox').css('background','none');
+  	oneisGray = false;
+  } else { 
+  	$('.firstbox').css('background','gray');
+    oneisGray = true;
+  }
+  console.log(oneisGray);
+})
+var twoisGray = true;
+console.log(twoisGray);
+$('.secondbox').on('click', function() {
+	if (twoisGray == true) {
+  	$('.secondbox').css('background','none');
+  	twoisGray = false;
+  } else { 
+  	$('.secondbox').css('background','gray');
+    twoisGray = true;
+  }
+  console.log(twoisGray);
+})
+var threeisGray = true;
+console.log(threeisGray);
+$('.thirdbox').on('click', function() {
+	if (threeisGray == true) {
+  	$('.thirdbox').css('background','none');
+  	threeisGray = false;
+  } else { 
+  	$('.thirdbox').css('background','gray');
+    threeisGray = true;
+  }
+  console.log(threeisGray);
+})
+var fourisGray = true;
+console.log(fourisGray);
+$('.fourthbox').on('click', function() {
+	if (fourisGray == true) {
+  	$('.fourthbox').css('background','none');
+  	fourisGray = false;
+  } else { 
+  	$('.fourthbox').css('background','gray');
+    fourisGray = true;
+  }
+  console.log(fourisGray);
+})
+var fiveisGray = true;
+console.log(fiveisGray);
+$('.fifthbox').on('click', function() {
+	if (fiveisGray == true) {
+  	$('.fifthbox').css('background','none');
+  	fiveisGray = false;
+  } else { 
+  	$('.fifthbox').css('background','gray');
+    fiveisGray = true;
+  }
+  console.log(fiveisGray);
+})
+var sixisGray = true;
+console.log(sixisGray);
+$('.sixthbox').on('click', function() {
+	if (sixisGray == true) {
+  	$('.sixthbox').css('background','none');
+  	sixisGray = false;
+  } else { 
+  	$('.sixthbox').css('background','gray');
+    sixisGray = true;
+  }
+  console.log(sixisGray);
+})
+var sevenisGray = true;
+console.log(sevenisGray);
+$('.seventhbox').on('click', function() {
+	if (sevenisGray == true) {
+  	$('.seventhbox').css('background','none');
+  	sevenisGray = false;
+  } else { 
+  	$('.seventhbox').css('background','gray');
+    sevenisGray = true;
+  }
+  console.log(sevenisGray);
+})
+var eightisGray = true;
+console.log(eightisGray);
+$('.eighthbox').on('click', function() {
+	if (eightisGray == true) {
+  	$('.eighthbox').css('background','none');
+  	eightisGray = false;
+  } else { 
+  	$('.eighthbox').css('background','gray');
+    eightisGray = true;
+  }
+  console.log(eightisGray);
+})
+var nineisGray = true;
+console.log(nineisGray);
+$('.ninthbox').on('click', function() {
+	if (nineisGray == true) {
+  	$('.ninthbox').css('background','none');
+  	nineisGray = false;
+  } else { 
+  	$('.ninthbox').css('background','gray');
+    nineisGray = true;
+  }
+  console.log(nineisGray);
+})
+var tenisGray = true;
+console.log(nineisGray);
+$('.tenthbox').on('click', function() {
+	if (tenisGray == true) {
+  	$('.tenthbox').css('background','none');
+  	tenisGray = false;
+  } else { 
+  	$('.tenthbox').css('background','gray');
+    tenisGray = true;
+  }
+  console.log(tenisGray);
+})
